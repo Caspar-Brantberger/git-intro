4fa9f6337b328bb847ff702d219cd936281622c3
Caspar-Brantberger <3tomu24.branca@folkuniversitetet.nu>
2025-03-10 10:00:55 +0100

    added execptionhandler

--- Spring/JPAEXAMINATION1/JPAEXAMINATION1/src/main/java/com/example/JPAEXAMINATION1/MyExecptionHandler.java
+++ Spring/JPAEXAMINATION1/JPAEXAMINATION1/src/main/java/com/example/JPAEXAMINATION1/MyExecptionHandler.java
@@ -1,4 +1,29 @@
 package com.example.JPAEXAMINATION1;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class MyExecptionHandler {
+
+
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,String> handleInvalidArguments(MethodArgumentNotValidException exceptions) {
+        Map<String, String> errorlist = new HashMap<>();
+
+        exceptions.getBindingResult()
+                .getFieldErrors()
+                .forEach(e -> errorlist.put(e.getField(), e.getDefaultMessage()));
+
+        return errorlist;
+    }
+
 }
